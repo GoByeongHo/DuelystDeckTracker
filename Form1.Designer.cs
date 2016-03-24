6c1e3f286afbec4372bd184e36218b25a8987836
GoByeongHo <tkxkd156*>
2016-03-25 00:30:34 +0900

    Create

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace DuelystDeckTracker
+{
+    partial class DuelstDeckTrecker
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DuelstDeckTrecker));
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.ResetBtn = new System.Windows.Forms.Button();
+            this.DivisionLine = new System.Windows.Forms.Panel();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.Total = new System.Windows.Forms.Label();
+            this.TopMostBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.SearchBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.SearchBox.Location = new System.Drawing.Point(5, 5);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(133, 21);
+            this.SearchBox.TabIndex = 0;
+            this.SearchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textEnter);
+            // 
+            // ResetBtn
+            // 
+            this.ResetBtn.Location = new System.Drawing.Point(123, 32);
+            this.ResetBtn.Name = "ResetBtn";
+            this.ResetBtn.Size = new System.Drawing.Size(55, 23);
+            this.ResetBtn.TabIndex = 1;
+            this.ResetBtn.Text = "Reset";
+            this.ResetBtn.UseVisualStyleBackColor = true;
+            this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // DivisionLine
+            // 
+            this.DivisionLine.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.DivisionLine.Location = new System.Drawing.Point(5, 60);
+            this.DivisionLine.Name = "DivisionLine";
+            this.DivisionLine.Size = new System.Drawing.Size(200, 2);
+            this.DivisionLine.TabIndex = 2;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(5, 32);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(55, 23);
+            this.SaveBtn.TabIndex = 3;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(64, 32);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(55, 23);
+            this.LoadBtn.TabIndex = 4;
+            this.LoadBtn.Text = "Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(144, 11);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(51, 12);
+            this.Total.TabIndex = 5;
+            this.Total.Text = "Total : 0";
+            // 
+            // TopMostBtn
+            // 
+            this.TopMostBtn.Location = new System.Drawing.Point(182, 32);
+            this.TopMostBtn.Name = "TopMostBtn";
+            this.TopMostBtn.Size = new System.Drawing.Size(23, 23);
+            this.TopMostBtn.TabIndex = 6;
+            this.TopMostBtn.Text = "D";
+            this.TopMostBtn.UseVisualStyleBackColor = true;
+            this.TopMostBtn.Click += new System.EventHandler(this.TopMostBtn_Click);
+            // 
+            // DuelstDeckTrecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(209, 103);
+            this.Controls.Add(this.TopMostBtn);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.DivisionLine);
+            this.Controls.Add(this.ResetBtn);
+            this.Controls.Add(this.SearchBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DuelstDeckTrecker";
+            this.Text = "DuelstDeckTrecker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Button ResetBtn;
+        private System.Windows.Forms.Panel DivisionLine;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.Button TopMostBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuelystDeckTracker
+{
+    public partial class DuelstDeckTrecker : Form
+    {
+        Dictionary<string, int> currentDeck = new Dictionary<string, int>();
+        Dictionary<string, int> viewDeck;
+
+        List<Button> buttonList = new List<Button>();
+        
+        public DuelstDeckTrecker()
+        {
+            InitializeComponent();
+            CardListLoad();
+        }
+
+        public void CardListLoad()
+        {
+            using(System.IO.StreamReader sr = 
+                new System.IO.StreamReader("CardList.txt"))
+            { 
+                while(true)
+                {
+                    string cardName = sr.ReadLine();
+                    if (cardName == null)
+                        return;
+                    SearchBox.AutoCompleteCustomSource.Add(cardName);
+                }
+            }
+        }
+
+        private void textEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddDeck(SearchBox.Text);
+            }
+        }
+
+        private void AddDeck(string _cardName)
+        {
+            if (currentDeck.ContainsKey(_cardName))
+            {
+                if (currentDeck[_cardName] == 3)
+                    return;
+                currentDeck[_cardName] += 1;
+                this.Controls.Find(_cardName, false)[0].Text = _cardName + "\nx" + currentDeck[_cardName];
+            }
+            else
+            {
+                currentDeck.Add(_cardName, 1);
+
+                ResetDeck();
+            }
+        }
+
+        private void CreateCardButton(string _cardName, int _count)
+        {
+            Button cardBtn = new Button();
+
+            cardBtn.Name = _cardName;
+            cardBtn.Text = "        "+_cardName + "\nx" + currentDeck[_cardName];
+            cardBtn.TextAlign = ContentAlignment.MiddleRight;
+            cardBtn.Location = new Point(5, 25 + 42 * _count);
+            cardBtn.Height = 44;
+            cardBtn.Width = 175;
+
+            if(System.IO.File.Exists("Resource\\" + _cardName.Trim().Replace(" ", "") + ".png"))
+                cardBtn.Image = new Bitmap("Resource\\" + _cardName.Trim().Replace(" ", "") + ".png");
+            cardBtn.ImageAlign = ContentAlignment.MiddleLeft;
+
+            cardBtn.MouseClick += new MouseEventHandler(CardBtn_Click);
+            this.Controls.Add(cardBtn);
+
+            buttonList.Add(cardBtn);
+
+            Button cardDeleteBtn = new Button();
+            cardDeleteBtn.Name = "Minus"+_cardName;
+            cardDeleteBtn.Text = "-";
+            cardDeleteBtn.Location = new Point(180, 25 + 42 * _count);
+            cardDeleteBtn.Height = 44;
+            cardDeleteBtn.Width = 25;
+            cardDeleteBtn.MouseClick += new MouseEventHandler(CardDeleteBtn_Click);
+            this.Controls.Add(cardDeleteBtn);
+
+            buttonList.Add(cardDeleteBtn);
+        }
+
+        private void RenewalCardButton()
+        {
+            int count = 0;
+            int total = 0;
+
+            foreach(KeyValuePair<string,int> card in currentDeck)
+            {
+                CreateCardButton(card.Key, ++count);
+
+                total += card.Value;
+            }
+
+            Total.Text = "Total : " + total;
+            viewDeck = new Dictionary<string,int>(currentDeck);
+        }
+
+        private void ResetDeck()
+        {
+            foreach(Button btn in buttonList)
+            {
+                btn.Dispose();
+            }
+
+            RenewalCardButton();
+        }
+
+        private void CardBtn_Click(object sender, EventArgs e)
+        {
+            Button sendButton = sender as Button;
+
+            if (viewDeck[sendButton.Name] == 0)
+                return;
+
+            viewDeck[sendButton.Name] -= 1;
+            this.Controls.Find(sendButton.Name, false)[0].Text = sendButton.Name + "\nx" + viewDeck[sendButton.Name];
+            if (viewDeck[sendButton.Name] == 0)
+                this.Controls.Find(sendButton.Name, false)[0].BackColor = Color.Gray;
+        }
+
+        private void CardDeleteBtn_Click(object sender, EventArgs e)
+        {
+            Button sendButton = sender as Button;
+
+            currentDeck.Remove(sendButton.Name.Remove(0, 5));
+
+            ResetDeck();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            using(SaveFileDialog saveFileDialog = new SaveFileDialog())
+            { 
+                saveFileDialog.Filter = "Deck|*.deck";
+                saveFileDialog.Title = "Save a Deck";
+                saveFileDialog.ShowDialog();
+
+                if (saveFileDialog.FileName != "")
+                {
+                    System.IO.FileStream fs =
+                        (System.IO.FileStream)saveFileDialog.OpenFile();
+
+                    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter format =
+                        new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    format.Serialize(fs, currentDeck);
+                }
+            }
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Deck|*.deck";
+                openFileDialog.Title = "Load a Deck";
+                openFileDialog.ShowDialog();
+
+
+                if (openFileDialog.FileName != "")
+                {
+                    System.IO.FileStream fs =
+                        (System.IO.FileStream)openFileDialog.OpenFile();
+
+                    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter format =
+                        new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                    object read = format.Deserialize(fs);
+                    currentDeck = read as Dictionary<string, int>;
+
+                    ResetDeck();
+                }
+            }
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            ResetDeck();
+        }
+
+        private void TopMostBtn_Click(object sender, EventArgs e)
+        {
+            if (TopMost == true)
+            {
+                TopMost = false;
+                TopMostBtn.Text = "D";
+                TopMostBtn.BackColor = SystemColors.Control;
+            }
+            else if (TopMost == false)
+            {
+                TopMost = true;
+                TopMostBtn.Text = "T";
+                TopMostBtn.BackColor = Color.Gray;
+            }
+        }
+    }
+}
